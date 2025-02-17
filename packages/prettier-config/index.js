26485abadbf8197f29246af21284211980b0d366
Armand Philippot <git@armand.philippot.eu>
2025-02-17 17:22:51 +0100

    feat: add prettier-config package (#7)
    * feat: add prettier-config package
    * docs: add changeset

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  objectWrap: "preserve",
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
+
+export default config;
