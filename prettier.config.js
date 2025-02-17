4fe551cad9af036e4ba4a2b52bd3bd54f92671a7
Armand Philippot <git@armand.philippot.eu>
2025-02-17 18:08:25 +0100

    chore: use workspace prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,9 @@
+import arphiPrettierConfig from "@arphi/prettier-config";
+
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-export default {};
+export default {
+  ...arphiPrettierConfig,
+};
