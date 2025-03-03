2e673716b2b8a26fd8eaabde3739e9ab01c6153f
Armand Philippot <git@armand.philippot.eu>
2025-03-03 19:26:16 +0100

    feat: configure comments rules

--- packages/eslint-config/src/configs/comments.ts
+++ packages/eslint-config/src/configs/comments.ts
@@ -12,9 +12,16 @@ export function comments(rulesOverrides: RulesOverrides = {}): Config[] {
     {
       name: "arphi/comments",
       plugins: {
-        comments: commentsPlugin,
+        "@eslint-community/eslint-comments": commentsPlugin,
       },
       rules: {
+        "@eslint-community/eslint-comments/disable-enable-pair": "error",
+        "@eslint-community/eslint-comments/no-aggregating-enable": "error",
+        "@eslint-community/eslint-comments/no-duplicate-disable": "error",
+        "@eslint-community/eslint-comments/no-unlimited-disable": "error",
+        "@eslint-community/eslint-comments/no-unused-disable": "error",
+        "@eslint-community/eslint-comments/no-unused-enable": "error",
+        "@eslint-community/eslint-comments/require-description": "warn",
         ...rulesOverrides,
       },
     },
